6006d1c6611874e3bbef2ff517a5856b0666a9e9
Deyán G. Sabogal <deyanfabricioguacaransabogal@MacBook-Pro-de-Deyan-Fabricio-Guacaran-Sabogal.local>
2020-06-27 11:09:26 +0100

    Add grid style on VideoDetail and VideoList

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
       params: {
         q: term,
         part: "snippet",
+        type: "video",
         maxResults: 5,
         key: KEY,
       },
@@ -33,11 +34,19 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={this.onVideoSelect}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -5,6 +5,7 @@ const VideoList = (props) => {
   const renderedList = props.videos.map((video) => {
     return (
       <VideoItem
+        key={video.id.videoId}
         thumbnailURL={video.snippet.thumbnails.medium.url}
         title={video.snippet.title}
         alt={video.snippet.description}
